9e4d9dbf8f140ae7cd95f7997d4e58fb9b613265
Daniel Ball <me.danielball98@gmail.com>
2022-05-12 19:59:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProjectUnreal
+
+Developed with Unreal Engine 4
---
+++ Source/ProjectUnreal/ProjectUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTUNREAL_API AProjectUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
